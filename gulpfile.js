379344661776e8702c9f1ec877a5e734fe3281d8
Adam Martin <im.adam.martin@gmail.com>
2015-03-22 01:09:55 +0700

    init deploy

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var gulpsync = require('gulp-sync')(gulp);
+var shell = require('gulp-shell');
+var markdown = require('gulp-markdown');
+
+gulp.task('build', function (cb) {
+  return gulp.src('./notes/index.md')
+    .pipe(markdown())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('deploy', shell.task([
+  'surge ./dist -d adam.surge.sh'
+]));
+
+gulp.task('default', gulpsync.sync(['build', 'deploy']));
